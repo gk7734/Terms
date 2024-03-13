ce49f82cc17b8f486e9d0c1f8f26f3d1e9b86314
gk7734 <127005459+gk7734@users.noreply.github.com>
2024-03-13 17:39:06 +0900

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div
-            className={`justify-center grid w-auto font-pretendard text-start bg-gradient-to-r from-white to-orange-300 ml-[10px] mr-[10px]`}>
+            className={`justify-center grid w-auto font-pretendard text-start bg-gradient-to-r from-white to-orange-300 pl-[10px] pr-[10px]`}>
             <h1 className={`text-[34px] font-bold text-black pt-8`}>Boom 개인 정보 보호 정책</h1>
             <br/>
             <h2 className={`font-semibold text-[20px] pb-7`}>2024년 3월 11일자로 발효</h2>
